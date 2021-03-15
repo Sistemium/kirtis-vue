87c568e629bf31186919c47dc7461b154c1c2398
Alexander Levin <alevin.ru@gmail.com>
2021-03-15 22:05:23 +0200

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+process.env.VUE_APP_VERSION = require('./package.json').version;
+
+module.exports = {
+
+  publicPath: '',
+
+  transpileDependencies: [
+    // 'sistemium-vue',
+  ],
+
+  productionSourceMap: false,
+
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://kirtis.info',
+        changeOrigin: true,
+        // pathRewrite: {
+        //   '^/api/dr50': '/apis',
+        // },
+      },
+    },
+  },
+
+};
