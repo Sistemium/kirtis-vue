7d54e75443310f761ddb00c3823e8450b80b6037
Alexander Levin <alevin.ru@gmail.com>
2021-03-15 14:01:03 +0200

    pugify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,13 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+
+.home
+  img(alt="Vue logo" src="../assets/kirtis-180.png")
+  HelloWorld(msg="Welcome to Your Vue.js App")
+
 </template>
 
 <script>
-// @ is an alias to /src
+
 import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
@@ -15,4 +16,5 @@ export default {
     HelloWorld,
   },
 };
+
 </script>
