98fd93b374d2e11559e77dc24d1b08b5603b2f3d
Alexander Levin <alevin.ru@gmail.com>
2021-03-15 22:06:04 +0200

    libs setup

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 import Vue from 'vue';
 
+import './lib/element-ui';
+import './lib/debug';
+
 import './index.scss';
-import App from './App.vue';
 import './registerServiceWorker';
+
 import router from './router';
 import store from './store';
+import App from './App.vue';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: h => h(App),
 }).$mount('#app');
