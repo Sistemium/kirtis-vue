404e8295a205c8d90cedea1cb38183793de0cfed
Alexander Levin <alevin.ru@gmail.com>
2021-03-16 14:18:51 +0200

    about (eng)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,24 @@
 <template lang="pug">
 
 .hello
+
   h1 {{ msg }}
-  p This project is written in
+
+  p This
     = ' '
-    a(href="https://vuejs.org" target="_blank" rel="noopener") VueJS
+    a(href="https://github.com/Sistemium/kirtis-vue" target="_blank") open-source
+    = ' '
+    | app is written in
+    = ' '
+    a(href="https://vuejs.org" target="_blank") VueJS
+    = ' '
+    | by
+    = ' '
+    a(href="https://sistemium.com" target="_blank") Sistemium UAB
+    = ' '
+    | courtesy of
+    = ' '
+    a(href="https://klc.vdu.lt" target="_blank") VDU KLC
 
 </template>
 <script>
